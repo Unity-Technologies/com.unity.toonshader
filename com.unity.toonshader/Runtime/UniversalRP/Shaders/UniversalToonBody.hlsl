8fc9916526e73ac5035d1d2fe2a00a780a737da9
Toshiyuki Mori <toshiyuki@unity3d.com>
2021-07-31 20:54:14 +0900

    Deleted #ifdef in univform variable declaration part of UniveraslToonBody.hlsl

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
@@ -151,21 +151,13 @@
             uniform float _Offset_Y_Axis_BLD;
             uniform fixed _Inverse_Z_Axis_BLD;
 //v.2.0.4
-#ifdef _IS_CLIPPING_MODE
-//DoubleShadeWithFeather_Clipping
-            uniform sampler2D _ClippingMask; uniform float4 _ClippingMask_ST;
-            uniform float _Clipping_Level;
-            uniform fixed _Inverse_Clipping;
-#elif defined(_IS_CLIPPING_TRANSMODE) || defined(_IS_TRANSCLIPPING_ON)
-//DoubleShadeWithFeather_TransClipping
             uniform sampler2D _ClippingMask; uniform float4 _ClippingMask_ST;
             uniform fixed _IsBaseMapAlphaAsClippingMask;
             uniform float _Clipping_Level;
             uniform fixed _Inverse_Clipping;
             uniform float _Tweak_transparency;
-#elif defined(_IS_CLIPPING_OFF) || defined(_IS_TRANSCLIPPING_OFF)
-//DoubleShadeWithFeather
-#endif
+
+
             // RaytracedHardShadow
 #define UNITY_PROJ_COORD(a) a
 #define UNITY_SAMPLE_SCREEN_SHADOW(tex, uv) tex2Dproj( tex, UNITY_PROJ_COORD(uv) ).r
