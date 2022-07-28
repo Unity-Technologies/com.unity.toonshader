d0768292dd4b1a1d75788fa51a2270129a032bc7
toshiyuki mori <toshiyuki@unity3d.com>
2022-07-28 18:16:07 +0900

    On the way to implement converter for built-in rp. [skip ci]

--- com.unity.toonshader/Editor/Converter/FromUTS2/BuiltInUTS2toIntegratedConverter.cs
+++ com.unity.toonshader/Editor/Converter/FromUTS2/BuiltInUTS2toIntegratedConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,121 @@ namespace UnityEditor.Rendering.Toon
         public override string info => "This tool converts project materials from Unity-chan Toon Shader to Unity Toon Shader " + UTS3Converter.versionString;
         public override int priority => -9000;
 
-        public override void SetupConverter() { }
+        public override void SetupConverter() {
+            bool isUts2Installed = CheckUTS2isInstalled();
+            bool isUts2SupportedVersion = CheckUTS2VersionError();
+        }
         public override void Convert() { }
         public override void PostConverting() { }
+
+        static int s_materialCount = 0;
+        static string[] s_guids;
+        static int s_versionErrorCount = 0;
+        const string legacyShaderPrefix = "UnityChanToonShader/";
+
+        static bool CheckUTS2VersionError()
+        {
+            s_guids = AssetDatabase.FindAssets("t:Material", null);
+            int materialCount = 0;
+
+            for (int ii = 0; ii < s_guids.Length; ii++)
+            {
+                var guid = s_guids[ii];
+
+
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                Material material = AssetDatabase.LoadAssetAtPath<Material>(path);
+
+                var shaderName = material.shader.ToString();
+                if (!shaderName.StartsWith(legacyShaderPrefix))
+                {
+                    continue;
+
+                }
+                const string utsVersionProp = "_utsVersion";
+                if (material.HasProperty(utsVersionProp))
+                {
+                    float utsVersion = material.GetFloat(utsVersionProp);
+                    if (utsVersion < 2.07)
+                    {
+                        s_versionErrorCount++;
+                        continue;
+                    }
+                }
+                else
+                {
+                    s_versionErrorCount++;
+                    continue;
+                }
+                materialCount++;
+            }
+            s_materialCount = materialCount;
+            if (s_versionErrorCount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool CheckUTS2isInstalled()
+        {
+            var shaders = AssetDatabase.FindAssets("t:Shader", new string[] { "Assets" });
+            foreach (var guid in shaders)
+            {
+                foreach (var shader in UTS2ShaderInfo.stdShaders)
+                {
+                    if (guid == shader.m_Guid)
+                    {
+                        /*
+                                            var filename = AssetDatabase.GUIDToAssetPath(guid);
+
+                                            if (!filename.EndsWith(kLegacyShaderFileName + kShaderFileNameExtention))
+                                            {
+                                                return true;
+                                            }
+                        */
+                        return true;
+
+                    }
+                }
+                foreach (var shader in UTS2ShaderInfo.tessShaders)
+                {
+                    if (guid == shader.m_Guid)
+                    {
+                        /*
+                                            var filename = AssetDatabase.GUIDToAssetPath(guid);
+
+                                            if (!filename.EndsWith(kLegacyShaderFileName + kShaderFileNameExtention))
+                                            {
+                                                return true;
+                                            }
+                        */
+                        return true;
+
+                    }
+                }
+            }
+            return false;
+        }
+        static UTSGUID FindUTS2GUID(string guid)
+        {
+            var ret = Array.Find<UTSGUID>(UTS2ShaderInfo.stdShaders, element => element.m_Guid == guid);
+            foreach (var shader in UTS2ShaderInfo.stdShaders)
+            {
+                if (shader.m_Guid == guid)
+                {
+                    return shader;
+                }
+            }
+            foreach (var shader in UTS2ShaderInfo.tessShaders)
+            {
+                if (shader.m_Guid == guid)
+                {
+                    return shader;
+                }
+            }
+            return null;
+        }
+
     }
 }
