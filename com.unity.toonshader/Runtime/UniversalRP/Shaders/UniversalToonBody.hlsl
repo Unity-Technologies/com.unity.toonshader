79c3fd44973a2595fd7ef57115c3ff7a680c49fd
h3idi-X <toshiyuki@unity3d.com>
2024-07-07 09:57:33 +0900

    Fixed: UNITY_VERSION string in the shader.

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
@@ -509,7 +509,7 @@
                 half fogFactor = ComputeFogFactor(positionCS.z);
 
                 OUTPUT_LIGHTMAP_UV(v.lightmapUV, unity_LightmapST, o.lightmapUV);
-#if UNITY_VERSION >= 600009
+#if UNITY_VERSION >= 60000009
                 OUTPUT_SH4(positionWS, o.normalDir.xyz, GetWorldSpaceNormalizeViewDir(positionWS), o.vertexSH, o.vertexSH);
 #elif UNITY_VERSION >= 202317
                 OUTPUT_SH4(positionWS, o.normalDir.xyz, GetWorldSpaceNormalizeViewDir(positionWS), o.vertexSH);
