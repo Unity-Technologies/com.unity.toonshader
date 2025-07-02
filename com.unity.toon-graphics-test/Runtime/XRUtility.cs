baa8dcd7ecdcbe0e7519ab1e38ff3cbbc88b167b
sin <sindharta@gmail.com>
2025-07-02 19:10:56 +0900

    XRUtility

---
+++ com.unity.toon-graphics-test/Runtime/XRUtility.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine.XR;
+using UnityEngine.XR.Management;
+
+namespace Unity.ToonShader.GraphicsTest {
+    
+public static class XRUtility {
+
+public static IEnumerator EnableXRCoroutine()
+{
+    if (XRGeneralSettings.Instance.Manager.activeLoader ||
+        XRGeneralSettings.Instance.Manager.isInitializationComplete)
+    {
+        DisableXR();
+        yield return null;
+    }
+
+    if (!XRGeneralSettings.Instance.Manager.activeLoader) {
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+    }
+
+    if (XRGeneralSettings.Instance.Manager.activeLoader 
+        && XRGeneralSettings.Instance.Manager.isInitializationComplete)
+    {
+        XRGeneralSettings.Instance.Manager.StartSubsystems();
+    }
+}
+
+
+public static void DisableXR() {
+
+    if (XRGeneralSettings.Instance.Manager.isInitializationComplete)
+    {
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+    }
+    
+    XRSettings.enabled = false;
+}
+    
+    
+}
+
+} //end namespace
