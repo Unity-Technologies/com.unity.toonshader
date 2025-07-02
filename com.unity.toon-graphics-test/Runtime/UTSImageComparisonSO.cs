e2038c00975bca05c31ec9f91385baf55530741b
sin <sindharta@gmail.com>
2025-07-02 10:29:46 +0900

    ImageComparisonSO

---
+++ com.unity.toon-graphics-test/Runtime/UTSImageComparisonSO.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.TestTools.Graphics;
+
+[Serializable]
+[CreateAssetMenu(menuName = "Unity Toon Shader/UTS Image Comparison Settings")]
+public class UTSImageComparisonSO : ScriptableObject
+{
+    [SerializeField] 
+    private ImageComparisonSettings m_imageComparisonSettings = new ImageComparisonSettings() {
+        TargetWidth = 960,
+        TargetHeight = 540,
+        PerPixelCorrectnessThreshold = 0.001f,
+        AverageCorrectnessThreshold = 0.005f,
+        UseHDR = false,
+        UseBackBuffer = false,
+        ImageResolution =  ImageComparisonSettings.Resolution.w1920h1080,
+    };
+    
+    public ImageComparisonSettings GetImageComparisonSettings() => m_imageComparisonSettings;
+}
+
