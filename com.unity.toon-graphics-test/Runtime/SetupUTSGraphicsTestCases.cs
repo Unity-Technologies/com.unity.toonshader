e60b123c81886be735c9a4ab2545b401d954afa2
sin <sindharta@gmail.com>
2025-07-02 19:14:38 +0900

    setup

---
+++ com.unity.toon-graphics-test/Runtime/SetupUTSGraphicsTestCases.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.TestTools;
+
+namespace Unity.ToonShader.GraphicsTest {
+    
+public class SetupUTSGraphicsTestCases : IPrebuildSetup {
+    public void Setup() {
+        XRUtility.DisableXR();
+    }
+}
+    
+} //end namespace
