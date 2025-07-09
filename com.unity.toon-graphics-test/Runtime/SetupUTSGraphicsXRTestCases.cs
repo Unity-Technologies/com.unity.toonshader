a8c27a50b1565ed2e13755b398320035969a9b34
sin <sindharta@gmail.com>
2025-07-09 09:45:34 +0900

    add XR

---
+++ com.unity.toon-graphics-test/Runtime/SetupUTSGraphicsXRTestCases.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.TestTools;
+
+namespace Unity.ToonShader.GraphicsTest {
+    
+public class SetupUTSGraphicsXRTestCases : IPrebuildSetup, IPostBuildCleanup {
+    public void Setup() {
+        
+        //Enable XR
+        XRUtility.EnableXR();
+    }
+    
+    public void Cleanup() {
+        XRUtility.DisableXR();
+    }
+}
+    
+} //end namespace
