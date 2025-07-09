1ef8d661196d5be3a03b18ca90928c68920bb90c
sin <sindharta@gmail.com>
2025-07-09 16:30:16 +0900

    move xr graphics test to editor

--- com.unity.toon-graphics-test/Runtime/UTS_GraphicsTests.cs
+++ com.unity.toon-graphics-test/Runtime/UTS_GraphicsTests.cs
@@ -7,46 +7,9 @@ using UnityEngine.TestTools.Graphics;
 using UnityEngine.SceneManagement;
 using System.IO;
 using Unity.ToonShader.GraphicsTest;
-using UnityEditor;
-
 
 namespace Tests
 {
-public class UTSGraphicsTestsXR {
-    
-    [UnityTest]
-    [UseGraphicsTestCases(UTSGraphicsTestConstants.ReferenceImagePath)]
-    [Timeout(3600000)] //1 hour
-    public IEnumerator Run(GraphicsTestCase testCase) {
-
-        //Enable XR
-        XRUtility.EnableXR();
-        
-#if UNITY_EDITOR
-        GameViewUtility.FindAndSelectSize(
-            1920, 1080, "Full HD (1920x1080)"
-        );
-#endif
-        string loadedXRDevice = UseGraphicsTestCasesAttribute.LoadedXRDevice;
-        
-        //Manually load the reference image for XR. Ex: URP/Linear/WindowsEditor/Vulkan/None/AngelRing.png
-        Assert.IsNotNull(testCase.ReferenceImage);
-        string imagePath = AssetDatabase.GetAssetPath(testCase.ReferenceImage);
-        string imageFileName = Path.GetFileName(imagePath);
-        string imageFolderName = Path.GetDirectoryName(Path.GetDirectoryName(imagePath));
-        Assert.IsNotNull(imageFolderName);
-        string xrImagePath = Path.Combine(imageFolderName, loadedXRDevice,imageFileName);
-        testCase.ReferenceImagePathLog = xrImagePath;
-        Assert.IsTrue(File.Exists(xrImagePath));
-        testCase.ReferenceImage = AssetDatabase.LoadAssetAtPath<Texture2D>(xrImagePath);
-        
-        //Unity.ToonShader.GraphicsTest.SetupUTSGraphicsXRTestCases.Setup();
-        yield return UTS_GraphicsTests.RunInternal(testCase, isXR:true);
-        
-        XRUtility.DisableXR();
-    }
-   
-} 
 public class UTSGraphicsTestsNonXR  {
     [UnityTest]
     [UseGraphicsTestCases(UTSGraphicsTestConstants.ReferenceImagePath)]
