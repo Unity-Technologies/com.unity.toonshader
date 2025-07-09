9a19cb5bccf0956a97f6ad760e96b55c0b93ff3e
sin <sindharta@gmail.com>
2025-07-09 10:26:24 +0900

    This works, need cleanup

--- com.unity.toon-graphics-test/Runtime/SetupUTSGraphicsXRTestCases.cs
+++ com.unity.toon-graphics-test/Runtime/SetupUTSGraphicsXRTestCases.cs
@@ -2,24 +2,31 @@
 using UnityEditor;
 #endif
 
+using Tests;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Unity.ToonShader.GraphicsTest {
     
-public class SetupUTSGraphicsXRTestCases : IPrebuildSetup, IPostBuildCleanup {
-    public void Setup() {
+public static class SetupUTSGraphicsXRTestCases {
+    public static void Setup() {
+        
+        Debug.Log("Setup XR: "+ UTS_GraphicsTests.number++);
         
         //Enable XR
         XRUtility.EnableXR();
         
+#if UNITY_EDITOR
         GameViewUtility.AddAndSelectCustomSize(
             GameViewUtility.GameViewSizeType.FixedResolution,
             GameViewSizeGroupType.Standalone,
-            1920, 1080, "Full HD (1920x1080)"
+            2*1920, 1080, "XR Full HD (3840x1080)"
         );
+#endif        
     }
     
-    public void Cleanup() {
+    public static void Cleanup() {
+        Debug.Log("Cleanup XR: "+ UTS_GraphicsTests.number++);
         XRUtility.DisableXR();
     }
 }
