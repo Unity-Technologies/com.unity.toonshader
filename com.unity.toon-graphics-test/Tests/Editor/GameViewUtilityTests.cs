5cb72e0928fc702d4263c55c97737d481e3fab9a
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2025-07-10 19:28:56 +0900

    Revert "Remove GameViewUtility"
    This reverts commit 1eba25b3ad8bade4d87d27729689efdfa7c69ee1.

---
+++ com.unity.toon-graphics-test/Tests/Editor/GameViewUtilityTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Unity.ToonShader.GraphicsTest.EditorTests {
+internal class GameViewUtilityTests {
+    
+    [Test]
+    public void CheckInitialization() {
+        Assert.IsTrue(GameViewUtility.IsInitialized());
+        
+        
+    }
+}
+} //end namespace
