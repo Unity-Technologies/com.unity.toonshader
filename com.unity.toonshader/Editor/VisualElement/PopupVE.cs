7b96ec963711f2e6a3a81d989ae1d6f8946bdeff
sin <sindharta@gmail.com>
2025-10-17 15:47:25 +0900

    Revert "fix errors for Unity 2022"
    This reverts commit 514f07abaf2d6650850047283285e154c1677c43.

--- com.unity.toonshader/Editor/VisualElement/PopupVE.cs
+++ com.unity.toonshader/Editor/VisualElement/PopupVE.cs
@@ -1,15 +1,7 @@
 using UnityEngine.UIElements;
 
 namespace UnityEditor.Rendering.Toon {
-    
-#if UNITY_6000_0_OR_NEWER
     [UxmlElement]
-#endif
     internal partial class PopupVE : PopupField<string> {
-
-#if !UNITY_6000_0_OR_NEWER
-        public new class UxmlFactory : UxmlFactory<PopupVE> {
-        }
-#endif
     }
 }
