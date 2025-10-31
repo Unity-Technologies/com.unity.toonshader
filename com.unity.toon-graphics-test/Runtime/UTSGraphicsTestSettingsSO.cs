d7408a5dff3fd0cabf0416b7dba10e30677434b9
sin <sindharta@gmail.com>
2025-10-31 11:22:27 +0900

    update SO

--- com.unity.toon-graphics-test/Runtime/UTSGraphicsTestSettingsSO.cs
+++ com.unity.toon-graphics-test/Runtime/UTSGraphicsTestSettingsSO.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine.TestTools.Graphics;
+﻿using System;
+using UnityEngine;
+using UnityEngine.TestTools.Graphics;
 
 namespace Unity.ToonShader.GraphicsTest {
-    public class UTSGraphicsTestSettingsSO {
-        public int WaitFrames = 0;
-        public ImageComparisonSettings ImageComparisonSettings;
+    
+    [CreateAssetMenu(fileName = "UTSGraphicsSettings", menuName = "Toon Shader/UTSGraphicsSettings")]
+    [Serializable]
+    public class UTSGraphicsTestSettingsSO : ScriptableObject {
+        public int WaitFrames = 480;
+        public ImageComparisonSettings ImageComparisonSettings = new ImageComparisonSettings();
     }
 }
