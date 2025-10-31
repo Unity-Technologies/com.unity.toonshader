28d809d2455af524d325ae76f3036e4dc7b6fab2
sin <sindharta@gmail.com>
2025-10-31 11:11:27 +0900

    add SO

---
+++ com.unity.toon-graphics-test/Runtime/UTSGraphicsTestSettingsSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.TestTools.Graphics;
+
+namespace Unity.ToonShader.GraphicsTest {
+    public class UTSGraphicsTestSettingsSO {
+        public int WaitFrames = 0;
+        public ImageComparisonSettings ImageComparisonSettings;
+    }
+}
