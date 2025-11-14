384e5939e3c23497a8ba048360960a86a4acff4e
sin <sindharta@gmail.com>
2025-11-14 22:24:09 +0900

    separate classes

---
+++ Toonshader_ProjectURP/Assets/Editor/Scripts/MaterialPropertyUIElement.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+internal class MaterialPropertyUIElement {
+    internal GUIContent label;
+    internal MaterialPropertyInfo mainProperty;
+    internal MaterialPropertyInfo extraProperty1;
+    internal MaterialPropertyInfo extraProperty2;
+}
+
