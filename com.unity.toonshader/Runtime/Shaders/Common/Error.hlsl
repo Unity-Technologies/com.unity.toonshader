8ca964087ea6ad7aedd0c81f63c2443472faf5ea
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2025-11-21 16:41:41 +0900

    dummy tesselation factors

--- com.unity.toonshader/Runtime/Shaders/Common/Error.hlsl
+++ com.unity.toonshader/Runtime/Shaders/Common/Error.hlsl
@@ -1,5 +1,6 @@
 #include <UnityCG.cginc>
 
+
 struct appdata {
     float4 vertex : POSITION;
 };
@@ -7,6 +8,26 @@ struct appdata {
 struct v2f {
     float4 vertex : SV_POSITION;
 };
+
+//----------------------------------------------------------------------------------------------------------------------
+struct DummyTessellationFactors
+{
+    float edge[3] : SV_TessFactor;
+    float inside : SV_InsideTessFactor;
+};
+
+[UNITY_domain("tri")]
+v2f ds_surf(DummyTessellationFactors factors, const OutputPatch<appdata, 3> patch, float3 bary : SV_DomainLocation) {
+ 
+   v2f o;
+    // Interpolate position using barycentric coordinates
+    o.vertex = patch[0].vertex * bary.x +
+            patch[1].vertex * bary.y +
+            patch[2].vertex * bary.z;
+    return o;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
 
 sampler2D _MainTex;
 float4 _MainTex_ST;
