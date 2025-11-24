907dc857c272eb313e32329344da89de04c3f726
sin <sindharta@gmail.com>
2025-11-24 12:57:42 +0900

    Revert "add CameraViewController"
    This reverts commit 6a428ffd65e742a1c83c58bca9ba683fa33e998f.

--- Toonshader_ProjectURP/Assets/Editor/Scripts/UnityToon3Das2DGUI.cs
+++ Toonshader_ProjectURP/Assets/Editor/Scripts/UnityToon3Das2DGUI.cs
@@ -143,7 +143,7 @@ class UnityToon3Das2DGUI : UnityEditor.ShaderGUI {
         
         EditorGUILayout.LabelField("Highlight Settings");
         EditorGUI.indentLevel+= INDENT_SIZE;
-        ToonEditorGUIUtility.DrawVector3FieldGUI(mEditor, mats, uiElements[Toon3Das2DConstants.SHADER_PROP_VIEW_DIRECTION]);
+        ToonEditorGUIUtility.DrawVector3FieldGUI(mEditor, mats, uiElements[SHADER_PROP_VIEW_DIRECTION]);
         ToonEditorGUIUtility.DrawTexturePropertySingleLineGUI(mEditor, uiElements[SHADER_PROP_HIGHLIGHT_TEX]);
         ToonEditorGUIUtility.DrawIntPopupGUI(mEditor, mats, uiElements[SHADER_PROP_DIRECTIONAL_LIGHT_HIGHLIGHT_MODE],
             m_highlightModeEnums, m_highlightModeIndices,out int _);
@@ -385,7 +385,7 @@ class UnityToon3Das2DGUI : UnityEditor.ShaderGUI {
                 "Multiplier for the diffuse contribution."),
         },
         new MaterialUIElement {
-            mainPropertyName = new MaterialName(Toon3Das2DConstants.SHADER_PROP_VIEW_DIRECTION),
+            mainPropertyName = new MaterialName(SHADER_PROP_VIEW_DIRECTION),
             label = new GUIContent("View Direction", "Camera View Direction"),
         },
         new MaterialUIElement {
@@ -502,6 +502,7 @@ class UnityToon3Das2DGUI : UnityEditor.ShaderGUI {
     internal const string SHADER_PROP_DIRECTIONAL_LIGHT_DIFFUSE_STRENGTH  = "_DirectionalLight_DiffuseStrength";
 
 
+    internal const string SHADER_PROP_VIEW_DIRECTION = "_ViewDirection";
     internal const string SHADER_PROP_HIGHLIGHT_COLOR = "_HighlightColor";
     internal const string SHADER_PROP_HIGHLIGHT_TEX = "_HighlightTex";
     internal const string SHADER_PROP_DIRECTIONAL_LIGHT_HIGHLIGHT_MODE = "_DirectionalLight_HighlightMode";
