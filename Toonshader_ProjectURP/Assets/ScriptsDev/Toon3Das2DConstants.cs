69c340f2be156a89b8b5927d97b58fce7af18c7a
sin <sindharta@gmail.com>
2025-11-24 12:20:18 +0900

    add CameraViewController

--- Toonshader_ProjectURP/Assets/ScriptsDev/Toon3Das2DConstants.cs
+++ Toonshader_ProjectURP/Assets/ScriptsDev/Toon3Das2DConstants.cs
@@ -4,4 +4,5 @@ public static class Toon3Das2DConstants {
     public const string ShaderPropUnlit_DirectionalLight_Color  = "_DirectionalLight_Color";
     public const string ShaderPropUnlit_DirectionalLight_Intensity  = "_DirectionalLight_Intensity";
     
+    public const string SHADER_PROP_VIEW_DIRECTION = "_ViewDirection";
 }
