42e1a88876a291394959494c9a0aa2300aa1126a
sin <sindharta@gmail.com>
2025-11-24 14:47:24 +0900

    Reapply "add CameraViewController"
    This reverts commit 0ff8a2766052708ebc8e8d8f47afbf894eff3421.

---
+++ Toonshader_ProjectURP/Assets/ScriptsDev/ToonCameraViewController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+internal class ToonCameraViewController : MonoBehaviour {
+    private void OnEnable() {
+        m_transform = transform;
+    }
+
+    void Update() {
+        Vector3 pos = m_transform.position;
+        foreach (Material mat in m_materials) {
+            if (mat == null) continue;
+            mat.SetVector(Toon3Das2DConstants.SHADER_PROP_DIRECTIONAL_LIGHT_VIEW_POSITION, pos);
+        }
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+    [SerializeField] private List<Material> m_materials = new List<Material>();
+
+    private Transform m_transform; 
+}
