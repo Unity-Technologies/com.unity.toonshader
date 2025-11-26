d0a5bfb2c70198bd650e0a1c590db9a1b059a940
sin <sindharta@gmail.com>
2025-11-26 16:25:07 +0900

    Remove unused code

--- Toonshader_ProjectURP/Assets/Shaders/ToonBlend.hlsl
+++ Toonshader_ProjectURP/Assets/Shaders/ToonBlend.hlsl
@@ -3,17 +3,6 @@
 
 #include "Packages/com.unity.render-pipelines.universal/Shaders/2D/Include/LightingUtility.hlsl" //_ShapeLightTexture
 
-
-float3 ToonDiffuseBlend(float3 shapeLight, float _ShapeLightBlendFactors) {
-
-    const float3 directionalLightColorAndUse = _DirectionalLight_Color * _DirectionalLight_Use; 
-
-    const float3 diffuseLightFactor = (shapeLight.rgb * _ShapeLightBlendFactors * _2DLightStrength )
-        + (directionalLightColorAndUse * _DirectionalLight_DiffuseStrength);
-
-    return diffuseLightFactor;
-}
-
 struct ShapeLightResult {
     half4 mod;
     half4 add;
