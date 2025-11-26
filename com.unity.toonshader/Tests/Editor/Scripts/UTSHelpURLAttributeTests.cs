cf42210bc5d5a5c558e3e6507f7a262f164a4f5a
sin <sindharta@gmail.com>
2025-11-27 02:04:35 +0900

    fix test

--- com.unity.toonshader/Tests/Editor/Scripts/UTSHelpURLAttributeTests.cs
+++ com.unity.toonshader/Tests/Editor/Scripts/UTSHelpURLAttributeTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Unity.Rendering.Toon;
 using System.Collections;
+using Unity.FilmInternalUtilities.Editor;
 using UnityEngine.TestTools;
 
 namespace Unity.ToonShader.EditorTests {
@@ -12,11 +13,11 @@ internal class UTSHelpURLAttributeTests {
         
         //check package version
         bool done = false;
-        string packageVersion = null;
-        FilmInternalUtilities.Editor.PackageUtility.FindInstalledPackageVersion(
+        string installedPackageVersionStr = null;
+        PackageUtility.FindInstalledPackageVersion(
             ToonConstants.PACKAGE_NAME,
             onVersionFound: (version) => {
-                packageVersion = version;
+                installedPackageVersionStr = version;
                 done = true;
             },
             onVersionNotFound: () => {
@@ -26,9 +27,14 @@ internal class UTSHelpURLAttributeTests {
         while (!done) {
             yield return null;
         }
-        Assert.IsFalse(string.IsNullOrEmpty(packageVersion));
+        Assert.IsFalse(string.IsNullOrEmpty(installedPackageVersionStr));
+
+        bool parseResult = PackageVersion.TryParse(installedPackageVersionStr, out PackageVersion packageVersion);
+        Assert.IsTrue(parseResult, $"Failed to parse package version string: {installedPackageVersionStr}");
         
-        Assert.IsTrue(ToonConstants.PACKAGE_VERSION_MAJOR_MINOR == packageVersion, 
+        string installedPackageVersionMajorMinor = packageVersion.GetMajor() + "." + packageVersion.GetMinor();
+        
+        Assert.IsTrue(ToonConstants.PACKAGE_VERSION_MAJOR_MINOR == installedPackageVersionMajorMinor, 
             $"Incorrect package version in {nameof(UTSHelpURLAttribute)}. " +
             $"Expected: " + packageVersion + " Actual: " + ToonConstants.PACKAGE_VERSION_MAJOR_MINOR);
 
