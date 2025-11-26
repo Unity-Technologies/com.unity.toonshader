626262e825326c9b06c7007b30ca46811b556f84
sin <sindharta@gmail.com>
2025-11-27 01:55:52 +0900

    reformat

--- com.unity.toonshader/Runtime/UTSHelpURLAttribute.cs
+++ com.unity.toonshader/Runtime/UTSHelpURLAttribute.cs
@@ -1,27 +1,20 @@
 using UnityEngine;
 
-namespace Unity.Rendering.Toon
-{
-    internal class UTSHelpURLAttribute : HelpURLAttribute
-    {
-        //[TODO-sin: 2025-10-20] Return the actual version
-        private const string fallbackVersion = "0.7";
+namespace Unity.Rendering.Toon {
+internal class UTSHelpURLAttribute : HelpURLAttribute {
+    //[TODO-sin: 2025-10-20] Return the actual version
+    private const string fallbackVersion = "0.7";
 
-        internal static string version
-        {
-            get
-            {
-                return fallbackVersion;
-            }
-        }
-        const string url = "https://docs.unity3d.com/Packages/{0}@{1}/manual/{2}.html";
-
-        internal UTSHelpURLAttribute(string pageName, string packageName = "com.unity.toonshader")
-            : base(GetPageLink(packageName, pageName))
-        {
-        }
-
-        internal static string GetPageLink(string packageName, string pageName) => string.Format(url, packageName, version, pageName);
-
+    internal static string version {
+        get { return fallbackVersion; }
     }
+
+    const string url = "https://docs.unity3d.com/Packages/{0}@{1}/manual/{2}.html";
+
+    internal UTSHelpURLAttribute(string pageName, string packageName = "com.unity.toonshader")
+        : base(GetPageLink(packageName, pageName)) {
+    }
+
+    internal static string GetPageLink(string packageName, string pageName) => string.Format(url, packageName, version, pageName);
+}
 }
