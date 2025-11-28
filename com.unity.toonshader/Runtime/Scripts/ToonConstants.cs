2f1ad70538fc8382c8c0343f693c5068f1163394
sin <sindharta@gmail.com>
2025-11-28 22:16:12 +0900

    ToonConstants

--- com.unity.toonshader/Runtime/Scripts/ToonConstants.cs
+++ com.unity.toonshader/Runtime/Scripts/ToonConstants.cs
@@ -81,11 +81,11 @@ internal static class ToonConstants {
     
     internal static readonly int SHADER_PROPERTY_MATERIAL_VERSION = Shader.PropertyToID("_ToonMaterialVersion");
     
-    public const string SHADER_PROP_DIRECTIONAL_LIGHT_DIRECTION = "_DirectionalLight_Direction";
-    public const string SHADER_PROP_DIRECTIONAL_LIGHT_COLOR  = "_DirectionalLight_Color";
-    public const string SHADER_PROP_DIRECTIONAL_LIGHT_INTENSITY  = "_DirectionalLight_Intensity";
+    internal const string SHADER_PROP_DIRECTIONAL_LIGHT_DIRECTION = "_DirectionalLight_Direction";
+    internal const string SHADER_PROP_DIRECTIONAL_LIGHT_COLOR  = "_DirectionalLight_Color";
+    internal const string SHADER_PROP_DIRECTIONAL_LIGHT_INTENSITY  = "_DirectionalLight_Intensity";
     
-    public const string SHADER_PROP_DIRECTIONAL_LIGHT_VIEW_POSITION = "_DirectionalLight_ViewPosition";
+    internal const string SHADER_PROP_DIRECTIONAL_LIGHT_VIEW_POSITION = "_DirectionalLight_ViewPosition";
     
     
     internal const string GBUFFER_PASS_NAME = "GBuffer";
