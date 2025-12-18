cfbc0c21da44868ccafb1e1786d2b4a5fe8e3b52
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2025-12-18 16:02:01 +0900

    remove comment

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonOutline.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonOutline.hlsl
@@ -1,8 +1,3 @@
-//Unity Toon Shader/Universal
-//nobuyuki@unity3d.com
-//toshiyuki@unity3d.com (Universal RP/HDRP)
-
-
             uniform float4 _LightColor0; // this is not set in c# code ?
 
             struct VertexInput {
