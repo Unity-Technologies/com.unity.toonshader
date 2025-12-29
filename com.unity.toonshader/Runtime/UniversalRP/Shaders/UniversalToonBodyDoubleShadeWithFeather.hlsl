36d52ca24835f5b93c7c1dc2b366acbdac8196b6
sin <sindharta@gmail.com>
2025-12-29 22:50:49 +0900

    highcolortex

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBodyDoubleShadeWithFeather.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBodyDoubleShadeWithFeather.hlsl
@@ -141,6 +141,8 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
 
     const float4 _Set_2nd_ShadePosition_var = tex2D(_Set_2nd_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_2nd_ShadePosition));
     const float4 _Set_1st_ShadePosition_var = tex2D(_Set_1st_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_1st_ShadePosition));
+    const float4 _Set_HighColorMask_var = tex2D(_Set_HighColorMask, TRANSFORM_TEX(Set_UV0, _Set_HighColorMask));
+    const float4 _HighColor_Tex_var = tex2D(_HighColor_Tex, TRANSFORM_TEX(Set_UV0, _HighColor_Tex));
     
 #ifdef _IS_PASS_FWDBASE
     float3 Set_LightColor = lightColor.rgb;
@@ -171,7 +173,6 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
                 _Set_2nd_ShadePosition_var.rgb).r - 1.0)) / (_ShadeColor_Step - (_ShadeColor_Step -
                 _1st2nd_Shades_Feather))))), Set_FinalShadowMask); // Final Color
 
-    const float4 _Set_HighColorMask_var = tex2D(_Set_HighColorMask, TRANSFORM_TEX(Set_UV0, _Set_HighColorMask));
 
     float _Specular_var = 0.5 * dot(halfDirection, lerp(i.normalDir, normalDirection, _Is_NormalMapToHighColor)) + 0.5;
     //  Specular
@@ -179,7 +180,6 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
         (1.0 - step(_Specular_var, (1.0 - pow(abs(_HighColor_Power), 5)))),
         pow(abs(_Specular_var), exp2(lerp(11, 1, _HighColor_Power))), _Is_SpecularToHighColor));
 
-    float4 _HighColor_Tex_var = tex2D(_HighColor_Tex, TRANSFORM_TEX(Set_UV0, _HighColor_Tex));
 
     float3 _HighColor_var = (lerp((_HighColor_Tex_var.rgb * _HighColor.rgb),
         ((_HighColor_Tex_var.rgb * _HighColor.rgb) * Set_LightColor),
@@ -386,8 +386,6 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
         (1.0 - step(_Specular_var, (1.0 - pow(_HighColor_Power, 5)))),
         pow(_Specular_var, exp2(lerp(11, 1, _HighColor_Power))), _Is_SpecularToHighColor));
 
-    float4 _HighColor_Tex_var = tex2D(_HighColor_Tex, TRANSFORM_TEX(Set_UV0, _HighColor_Tex));
-
     float3 _HighColor_var = (lerp((_HighColor_Tex_var.rgb * _HighColor.rgb),
         ((_HighColor_Tex_var.rgb * _HighColor.rgb) * Set_LightColor),
         _Is_LightColor_HighColor) * _TweakHighColorMask_var);
@@ -480,9 +478,7 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
     float _TweakHighColorMask_var = (saturate((_Set_HighColorMask_var.g + _Tweak_HighColorMaskLevel)) * lerp(
         (1.0 - step(_Specular_var, (1.0 - pow(_HighColor_Power, 5)))),
         pow(_Specular_var, exp2(lerp(11, 1, _HighColor_Power))), _Is_SpecularToHighColor));
-
-    float4 _HighColor_Tex_var = tex2D(_HighColor_Tex, TRANSFORM_TEX(Set_UV0, _HighColor_Tex));
-
+        
     float3 _HighColor_var = (lerp((_HighColor_Tex_var.rgb * _HighColor.rgb),
         ((_HighColor_Tex_var.rgb * _HighColor.rgb) * Set_LightColor),
         _Is_LightColor_HighColor) * _TweakHighColorMask_var);
