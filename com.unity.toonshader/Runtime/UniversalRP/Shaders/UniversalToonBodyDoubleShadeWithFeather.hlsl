f8a12a56b1d07cceafab47712a4ff1d4af3d56b8
sin <sindharta@gmail.com>
2026-01-16 21:32:26 +0900

    shadePos

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBodyDoubleShadeWithFeather.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBodyDoubleShadeWithFeather.hlsl
@@ -76,8 +76,8 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
     );
 
     const float4 mainTex = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, TRANSFORM_TEX(Set_UV0, _MainTex));
-    const float4 _Set_1st_ShadePosition_var = tex2D(_Set_1st_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_1st_ShadePosition));
-    const float4 _Set_2nd_ShadePosition_var = tex2D(_Set_2nd_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_2nd_ShadePosition));
+    const float4 firstShadePos = tex2D(_Set_1st_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_1st_ShadePosition));
+    const float4 secondShadePos = tex2D(_Set_2nd_ShadePosition, TRANSFORM_TEX(Set_UV0, _Set_2nd_ShadePosition));
     const float4 highlightTex = tex2D(_HighColor_Tex, TRANSFORM_TEX(Set_UV0, _HighColor_Tex));
     const float4 highlightMask = tex2D(_Set_HighColorMask, TRANSFORM_TEX(Set_UV0, _Set_HighColorMask));
 
@@ -163,14 +163,14 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
                                         : 0.0001;
     float Set_FinalShadowMask = saturate(
         (1.0 + ((lerp(_HalfLambert_var, _HalfLambert_var * saturate(_SystemShadowsLevel_var), _Set_SystemShadowsToBase)
-            - (_BaseColor_Step - _BaseShade_Feather)) * ((1.0 - _Set_1st_ShadePosition_var.rgb).r - 1.0)) / (
+            - (_BaseColor_Step - _BaseShade_Feather)) * ((1.0 - firstShadePos.rgb).r - 1.0)) / (
             _BaseColor_Step - (_BaseColor_Step - _BaseShade_Feather))));
     //
     //Composition: 3 Basic Colors as Set_FinalBaseColor
     float3 Set_FinalBaseColor = lerp(Set_BaseColor, lerp(Set_1st_ShadeColor, Set_2nd_ShadeColor,
         saturate(
             (1.0 + ((_HalfLambert_var - (_ShadeColor_Step - _1st2nd_Shades_Feather)) * ((1.0 -
-                _Set_2nd_ShadePosition_var.rgb).r - 1.0)) / (_ShadeColor_Step - (_ShadeColor_Step -
+                secondShadePos.rgb).r - 1.0)) / (_ShadeColor_Step - (_ShadeColor_Step -
                 _1st2nd_Shades_Feather))))), Set_FinalShadowMask); // Final Color
 
 
@@ -369,13 +369,13 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
     //v.2.0.5:
     float Set_FinalShadowMask = saturate(
         (1.0 + ((lerp(_HalfLambert_var, (_HalfLambert_var * saturate(1.0 + _Tweak_SystemShadowsLevel)),
-            _Set_SystemShadowsToBase) - (baseColorStep - _BaseShade_Feather)) * ((1.0 - _Set_1st_ShadePosition_var.rgb).
+            _Set_SystemShadowsToBase) - (baseColorStep - _BaseShade_Feather)) * ((1.0 - firstShadePos.rgb).
             r - 1.0)) / (baseColorStep - (baseColorStep - _BaseShade_Feather))));
     //Composition: 3 Basic Colors as finalColor
     float3 finalColor = lerp(Set_BaseColor, lerp(Set_1st_ShadeColor, Set_2nd_ShadeColor,
             saturate(
                 (1.0 + ((_HalfLambert_var - (shadeColorStep - _1st2nd_Shades_Feather)) * ((1.0 -
-                    _Set_2nd_ShadePosition_var.
+                    secondShadePos.
                     rgb).r - 1.0)) / (shadeColorStep - (shadeColorStep - _1st2nd_Shades_Feather))))),
         Set_FinalShadowMask); // Final Color
 
@@ -462,13 +462,13 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
     //v.2.0.5:
     float Set_FinalShadowMask = saturate(
         (1.0 + ((lerp(_HalfLambert_var, (_HalfLambert_var * saturate(1.0 + _Tweak_SystemShadowsLevel)),
-            _Set_SystemShadowsToBase) - (baseColorStep - _BaseShade_Feather)) * ((1.0 - _Set_1st_ShadePosition_var.rgb).
+            _Set_SystemShadowsToBase) - (baseColorStep - _BaseShade_Feather)) * ((1.0 - firstShadePos.rgb).
             r - 1.0)) / (baseColorStep - (baseColorStep - _BaseShade_Feather))));
     //Composition: 3 Basic Colors as finalColor
     float3 finalColor = lerp(Set_BaseColor, lerp(Set_1st_ShadeColor, Set_2nd_ShadeColor,
             saturate(
                 (1.0 + ((_HalfLambert_var - (shadeColorStep - _1st2nd_Shades_Feather)) * ((1.0 -
-                    _Set_2nd_ShadePosition_var.
+                    secondShadePos.
                     rgb).r - 1.0)) / (shadeColorStep - (shadeColorStep - _1st2nd_Shades_Feather))))),
         Set_FinalShadowMask); // Final Color
 
