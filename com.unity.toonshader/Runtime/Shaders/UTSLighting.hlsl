c0a2dbeaa9a94f1e383860013227e0407ed4c6d2
sin <sindharta@gmail.com>
2026-02-03 21:07:06 +0900

    test

--- com.unity.toonshader/Runtime/Shaders/UTSLighting.hlsl
+++ com.unity.toonshader/Runtime/Shaders/UTSLighting.hlsl
@@ -6,4 +6,29 @@ inline float Intensity(float3 lightColor)
     return 0.299 * lightColor.r + 0.587 * lightColor.g + 0.114 * lightColor.b;
 }
 
+//----------------------------------------------------------------------------------------------------------------------
+
+float3 ThreeColorsLinearShading(
+    float3 baseColor,
+    float3 firstColor,
+    float3 secondColor,
+    float3  baseTo1stStart,     // t=0: use base, t=1: transition
+    float3  baseTo1stFeather,
+    float3  firstToSecondStart, //t=0: use base, t=1: transition
+    float3  firstToSecondFeather,
+    float  dotNL) // dot(N.L)
+{
+    const float t = saturate(1 - dotNL); //t = 0: light, t=1: dark shaded
+
+    const float invBaseTo1stStart = 1 - baseTo1stStart;
+    const float invBaseTo2ndStart = 1 - firstToSecondStart;
+                
+    const float s1 = smoothstep(invBaseTo1stStart, invBaseTo1stStart + baseTo1stFeather,t); 
+    const float s2 = smoothstep(invBaseTo2ndStart, invBaseTo2ndStart + firstToSecondFeather,t); 
+                
+    float3 c01 = lerp(baseColor,firstColor,  s1);
+    float3 c12 = lerp(c01, secondColor, s2);
+    return c12;
+}
+
 #endif // UTS_LIGHTING_INCLUDED
