6f722ea4967d5cb6fa8526b6bc850788c3327a1b
sin <sindharta@gmail.com>
2026-02-25 22:56:34 +0900

    works. need cleanup

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
@@ -237,7 +237,8 @@ UtsLight GetUrpMainUtsLight(float4 shadowCoord) {
 // Fills a light struct given a loop i index. This will convert the i
 // index to a perObjectLightIndex
 UtsLight GetAdditionalUtsLight(uint i, float3 positionWS) {
-    Light light = GetAdditionalLight(i, positionWS);
+    const float shadowMask = 1;
+    Light light = GetAdditionalLight(i, positionWS, shadowMask);
 
     UtsLight utsLight;
     utsLight.direction = light.direction;
