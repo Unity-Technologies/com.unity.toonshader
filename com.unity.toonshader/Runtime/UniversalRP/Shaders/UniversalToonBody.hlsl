651484d72255b42778d19d65b607bd71c369be30
sin <sindharta@gmail.com>
2026-02-27 21:02:37 +0900

    do saturate

--- com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
+++ com.unity.toonshader/Runtime/UniversalRP/Shaders/UniversalToonBody.hlsl
@@ -275,7 +275,7 @@ float4 GetShadowCoordUTS(VertexOutput v)
 }
 
 inline float TweakShadow(float shadow) {
-    return max((shadow * 0.5) + 0.5 + _Tweak_SystemShadowsLevel, 0.001); 
+    return saturate(max((shadow * 0.5) + 0.5 + _Tweak_SystemShadowsLevel, 0.001)); 
 }
 
 //----------------------------------------------------------------------------------------------------------------------
