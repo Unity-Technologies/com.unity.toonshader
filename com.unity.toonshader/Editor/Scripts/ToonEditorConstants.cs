3665a4750f76584a5652d6f3b6f89d670856b875
sin <sindharta@gmail.com>
2026-04-08 21:27:07 +0900

    add constants

--- com.unity.toonshader/Editor/Scripts/ToonEditorConstants.cs
+++ com.unity.toonshader/Editor/Scripts/ToonEditorConstants.cs
@@ -9,6 +9,13 @@ internal static class ToonEditorConstants {
     internal const int CUR_MATERIAL_VERSION = (int) ToonMaterialVersion.Initial;
     
     internal static readonly string PACKAGE_PATH = Path.Combine("Packages", ToonConstants.PACKAGE_NAME).Replace('\\','/');
+    
+    
+    internal static readonly string TOON_SHADER_PATH = 
+        Path.Combine(PACKAGE_PATH,"Runtime/Shaders/UnityToon.shader").Replace('\\','/');
+    internal static readonly string TOON_TESS_SHADER_PATH = 
+        Path.Combine(PACKAGE_PATH,"Runtime/Shaders/UnityToonTessellation.shader").Replace('\\','/');
+    
 
 }
 
