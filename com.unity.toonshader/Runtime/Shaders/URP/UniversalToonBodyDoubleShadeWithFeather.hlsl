9696fdfd4179d803dd12fdf41834f03d4d6eb2f2
sin <sindharta@gmail.com>
2026-04-13 23:33:48 +0900

    fix: fog
    (cherry picked from commit 94e692af7826bc5076b791378de66517f837a48f)

--- com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBodyDoubleShadeWithFeather.hlsl
+++ com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBodyDoubleShadeWithFeather.hlsl
@@ -526,6 +526,8 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
 
 #endif
 
+    finalRGBA.rgb = MixFog(finalRGBA.rgb, inputData.fogCoord);
+
 #ifdef _WRITE_RENDERING_LAYERS
     uint renderingLayers = GetMeshRenderingLayer();
     outRenderingLayers = float4(EncodeMeshRenderingLayer(renderingLayers), 0, 0, 0);
