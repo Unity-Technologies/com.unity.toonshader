14f590af2ebe2c1117e747242a6a5801f2578635
sin <sindharta@gmail.com>
2026-05-01 22:18:48 +0900

    use the macro

--- com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBodyShadingGradeMap.hlsl
+++ com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBodyShadingGradeMap.hlsl
@@ -547,7 +547,6 @@ void frag(VertexOutput i, out float4 finalRGBA : SV_Target0
     finalRGBA.rgb = MixFog(finalRGBA.rgb, inputData.fogCoord);
 
 #ifdef _WRITE_RENDERING_LAYERS
-    uint renderingLayers = GetMeshRenderingLayer();
-    outRenderingLayers = float4(EncodeMeshRenderingLayer(renderingLayers), 0, 0, 0);
+    outRenderingLayers = float4(ENCODE_MESH_RENDERING_LAYER_UTS, 0, 0, 1);
 #endif
 }
