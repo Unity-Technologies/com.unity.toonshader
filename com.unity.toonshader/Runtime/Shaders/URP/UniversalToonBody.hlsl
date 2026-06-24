6b015a32b5469bdc024773682e0ed63652d31d1f
sin <sindharta@gmail.com>
2026-06-24 10:51:05 +0900

    remove commented out code

--- com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBody.hlsl
+++ com.unity.toonshader/Runtime/Shaders/URP/UniversalToonBody.hlsl
@@ -21,14 +21,6 @@
 # endif
 #endif
 
-//
-// #if USE_FORWARD_PLUS && defined(LIGHTMAP_ON) && defined(LIGHTMAP_SHADOW_MIXING)
-// #define FORWARD_PLUS_SUBTRACTIVE_LIGHT_CHECK if (_AdditionalLightsColor[lightIndex].a > 0.0h) continue;
-// #else
-// #define FORWARD_PLUS_SUBTRACTIVE_LIGHT_CHECK
-// #endif
-
-
 //function to rotate the UV: RotateUV()
 //float2 rotatedUV = RotateUV(i.uv0, (_angular_Verocity*3.141592654), float2(0.5, 0.5), _Time.g);
 float2 RotateUV(float2 _uv, float _radian, float2 _piv, float _time) {
