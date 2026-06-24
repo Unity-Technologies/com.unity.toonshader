f0e9ce03f1ba4ce70f5b9a4dd58a6186839cc6d3
sin <sindharta@gmail.com>
2026-06-24 16:53:46 +0900

    remove debug logs

--- com.unity.toon-graphics-test/Runtime/UTSGraphicsTests.cs
+++ com.unity.toon-graphics-test/Runtime/UTSGraphicsTests.cs
@@ -123,15 +123,6 @@ public class UTSGraphicsTestsNonXR  {
         internal static IEnumerator RunInternal(SceneGraphicsTestCase testCase, bool isXR = false) {
 
             SceneManager.LoadScene(testCase.ScenePath);
-
-            Debug.Log(testCase.ReferenceImage.Image);
-            Debug.Log(testCase.ReferenceImage.Image);
-            Debug.Log(testCase.ReferenceImage.Image);
-            
-            Debug.Log(testCase.ReferenceImage.AssetPath);
-            Debug.Log(testCase.ReferenceImage.AssetPath);
-            Debug.Log(testCase.ReferenceImage.AssetPath);
-            Debug.Log(testCase.ReferenceImage.AssetPath);
             
             // Always wait one frame for scene load
             yield return null;
