e8a9473e65f9726ac6b27f4eb90164660ad1dd05
sin <sindharta@gmail.com>
2026-06-24 23:12:41 +0900

    update constants

--- com.unity.toon-graphics-test/Runtime/UTSGraphicsTestConstants.cs
+++ com.unity.toon-graphics-test/Runtime/UTSGraphicsTestConstants.cs
@@ -1,14 +1,7 @@
 namespace Unity.ToonShader.GraphicsTest
 {
 public class UTSGraphicsTestConstants {
-#if UTS_TEST_USE_HDRP
-    internal const string ReferenceImagePath = "Packages/com.unity.toon-reference-images/HDRP";
-#elif UTS_TEST_USE_URP
-    internal const string ReferenceImagePath = "Packages/com.unity.toon-reference-images/URP";
-#else
-    internal const string ReferenceImagePath = "Packages/com.unity.toon-reference-images/Built-In";
-#endif
-
+    internal const string ReferenceImagePath = "Assets/ReferenceImages";
 }
 
 } //end namespace
